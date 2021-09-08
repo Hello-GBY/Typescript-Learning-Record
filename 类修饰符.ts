f587a57712e5bf23dfbecf88e903277edc69ac13
gby <1565297930@qq.com>
2021-09-08 20:28:16 +0800

    public protected private

---
+++ 类修饰符.ts
@@ -0,0 +1,23 @@
+/**
+ * 类的修饰符
+ *  public: 公有
+ *  
+ */
+
+ class Animal_2 {
+    public name: string;
+    protected sex: string; // 外部访问不了 只能在当前类 或者 子类 使用 
+    private age: string;   // 外部访问不了 只能在当前类使用 
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    run(): string{
+        return `${this.name}正在跑`
+    }
+}
+var cat_2 = new Animal_2('猫猫');
+cat_2.name;
+cat_2.sex; // 报错
+cat_2.age; // 报错
