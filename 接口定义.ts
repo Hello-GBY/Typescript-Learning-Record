8a99b0b82422c7b4a9f991cd7f678aef88b842ef
gby <nameGBY@163.com>
2021-09-10 00:06:26 +0800

    接口定义

--- 接口定义.ts
+++ 接口定义.ts
@@ -1,13 +1,19 @@
+// 接口：可选属性
 interface obj {
   name: string,
-  value: number
+  value?: number
 }
 
 function setObj(val: obj) {
 
 }
-
+// 参数的顺序可以不一样
 setObj({
-  name:'haha',
+  name:'XiXi',
   value: 123
-})
+});
+
+// 参数的可传可不传
+setObj({
+  name:'XiXi'
+});
