3efd555d895d1c00b0ca3fdd1d161a785e669354
gby <nameGBY@163.com>
2021-09-11 18:11:44 +0800

    接口

--- js/接口定义.js
+++ js/接口定义.js
@@ -1,4 +1,17 @@
 "use strict";
+/**
+ * 接口的作用：
+ *  定义标准、定义一种规范 只是定义不实现，类是定义并实现
+ *  关键字： implement (实现)
+ *
+ *
+ * 接口的类型：
+ *  可索引接口
+ *  函数接口
+ *  类类型接口
+ *  属性类接口
+ *  接口的扩展
+ */
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -10,6 +23,27 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var myFunc = function (v1, v2) {
+    console.log('v1, v2: ', v1, v2);
+    return '';
+};
+var arr = ['1', '2', '3'];
+console.log('arr[0]: ', arr[0]);
+var obj = {
+    'name': 'da',
+    'age': '123'
+};
+var Bard = /** @class */ (function () {
+    function Bard(name) {
+        this.name = name;
+    }
+    Bard.prototype.eat = function (val) {
+        return this.name + '会吃:' + val;
+    };
+    return Bard;
+}());
+// 接口扩展  接口继承  接口继承接口  extends
+myFunc('haha', 'xixi');
 function setObj(val) {
 }
 // 参数的顺序可以不一样
@@ -26,4 +60,4 @@ function ajax(config) {
     // let xml = new XMLHttpRequest();
 }
 console.log('asdf');
-alert(1);
+// alert(1);
--- ts/接口定义.ts
+++ ts/接口定义.ts
@@ -1,3 +1,69 @@
+/**
+ * 接口的作用：
+ *  定义标准、定义一种规范 只是定义不实现，类是定义并实现
+ *  关键字： implement (实现)
+ *  
+ * 
+ * 接口的类型：
+ *  可索引接口
+ *  函数接口
+ *  类类型接口
+ *  属性类接口
+ *  接口的扩展
+ */
+
+// 属性接口
+interface FullName {
+  firstName: '',
+  secondName?: ''
+}
+
+// 函数类型接口
+interface encrypt{
+  (name: string, value: string): string
+}
+
+var myFunc:encrypt= function(v1, v2){
+  console.log('v1, v2: ', v1, v2);
+  return ''
+}
+
+// 可索引接口 对 对象 和数组进行约束 不常用
+ // 对数组进行约束
+interface userArr{
+  [index: number]: string
+} // index 不能 家引号
+var arr:userArr = ['1','2','3'];
+console.log('arr[0]: ', arr[0]);
+  // 对对象进行约束
+interface userObj {
+  [index: string]: string
+}
+var obj:userObj = {
+  'name': 'da',
+  'age': '123'
+}
+
+// 类类型接口 对类进行约束 只是定义  定义方法时没有具体的实现
+interface userClass {
+  name:string,
+  eat(val: string):string
+}
+
+class Bard implements userClass {
+  name: string,
+  constructor(name:string) {
+    this.name = name;
+  }
+  eat(val: string) {
+    return this.name + '会吃:' + val
+  }
+}
+// 接口扩展  接口继承  接口继承接口  extends
+
+myFunc( 'haha', 'xixi')
+
+
 // 接口：可选属性
 interface obj {
   name: string,
@@ -34,5 +100,5 @@ function ajax (config: config){
 }
 
 console.log('asdf')
-alert(1);
+// alert(1);
 
