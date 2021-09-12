9da7ae73c490b8b5198255a870ed0d59ef86635a
gby <nameGBY@163.com>
2021-09-12 22:09:23 +0800

    装饰器

--- ts/装饰器.ts
+++ ts/装饰器.ts
@@ -1,40 +1,22 @@
 /**
  * 装饰器：
  *  是一种特殊的类型说明 能附加到类的声明、方法、属性或参数上、可以访问修改类的行为。
- *  
+ *  常见的装饰器  类装饰器 属性装饰器 构造函数装饰器器
  */
 
 // 装饰器 ：  类装饰器
-
-// 
-function logClass(params: string){
-  // return function () {
+// 1. 普通装饰器
+function logClass(params: any){
+  console.log('params: ', params);
+  params.prototype.apiUrl = 'xxxx';
+  params.prototype.run = function (){
     
-  // }
+  }
 }
-
-
-// 装饰器
 @logClass
 class HttpClient{
-  constructor(){
-
-  }
-  getData(){
-
-  }
+  constructor () {}
+  getData(){}
 }
-
-
-
-function logClass2(params: string){
-  console.log('params: ', params);
-  return function (target: any) {
-    console.log('target: ', target);
-  }
-}
-// 装饰器工厂 可传参
-@logClass2('hello')
-class HttpClient2{
-  constructor(){}
-}
+ 
+ var http = new HttpClient()
