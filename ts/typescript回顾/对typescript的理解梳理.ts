55ad285578a0740c35189541241afab92a8e804d
gby <nameGBY@163.com>
2021-09-30 16:34:33 +0800

    ts 基本类型梳理

--- ts/typescript回顾/对typescript的理解梳理.ts
+++ ts/typescript回顾/对typescript的理解梳理.ts
@@ -1,21 +1,6 @@
-// 数据基本类型
-let t_1: string
-let t_2: number
-let t_3: boolean
-let t_4: any
-let t_5: void
-let t_6: undefined | null
-interface t_7 {
-    name: string,
-    age: number,
-}
-enum t_8  {
-    name = 'XXX',
-    age = '18'
-}
-type t_9 = {
-    name: string,
-    age: number
-}
+// 基本类型
+    // 数字
+    // 数组
+    // 
 
 // 高级类型
--- ts/数据类型.ts
+++ ts/数据类型.ts
@@ -9,8 +9,32 @@
  *  never 
  *  enum  枚举
  *  tuple 元数组
+ *  对象类型
  */
-
+// 数据基本类型
+let t_1: string = '123'
+let t_2: number = 123
+let t_3: boolean = false
+let t_4: any   // 定义之后可以存放任意类型的值
+let t_5: void  // 用于方法的返回值，代表没有返回值
+let t_6: undefined | null = null
+interface t_7 {
+    name: string,
+    age: number,
+}
+enum t_8  {
+    name = 'XXX',
+    age = '18'
+}
+type t_9 = {
+    name: string,
+    age: number
+}
+// 注意
+let throwRes2: never; 
+let tupleArr2:[number, string, number]; // 元组
+let arrNew: string[] = ['q', 'w'];// let arr2: number[]
+let obj_ : object = {name: 'me', age : 'X'}
 
 // 数组类型定义
 // 第一种
@@ -39,6 +63,15 @@ var val:Flag2 = Flag2.blue;
 console.log('val: ', val); // 1
 
 
-// never 类型 代表从不会出现的值  null 或者 undefined  类型
+// never 类型 代表从不会出现的值   用于接收错误处理的结果
+// never 用于定义重来不会出现的类型 用于接收错误处理
+let a: never;
+// a=123 ; // 错误写法
+a = (() => {
+  throw new Error('数据处理错误')
+})()
 
+// 元组类型
+// 用于定义一组已知的数组
+let tupleArr:[number, string, number];
 
