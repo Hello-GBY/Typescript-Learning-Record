47b488914d7217ee1f475a3b57fa980cd7976e41
GuoBingYu <nameGBY@163.com>
2021-09-30 15:37:00 +0800

    tead()

---
+++ ts/typescript回顾/对typescript的理解梳理.md
@@ -0,0 +1,19 @@
+# 对typescript的理解 与 JavaScript的区别
+
+1. 超集
+   1. Typescript 是 javascript的超集，Typescript在JavaScript进行扩展 完全包含JavaScript语法。
+   2. 强类型语言
+
+2. 特性
+   1.   类型约束
+   2.   接口
+   3.   类型
+   4.   枚举
+   5. * ？ 泛型 
+   6. * ？ 名字空间
+   7. * ？ 元组
+   8. * ？ mixin
+   9. * ？ 装饰器
+
+3. 
+
---
+++ ts/typescript回顾/对typescript的理解梳理.ts
@@ -0,0 +1,21 @@
+// 数据基本类型
+let t_1: string
+let t_2: number
+let t_3: boolean
+let t_4: any
+let t_5: void
+let t_6: undefined | null
+interface t_7 {
+    name: string,
+    age: number,
+}
+enum t_8  {
+    name = 'XXX',
+    age = '18'
+}
+type t_9 = {
+    name: string,
+    age: number
+}
+
+// 高级类型
